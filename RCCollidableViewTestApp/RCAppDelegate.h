0f334d31df5033257b0bed1ef7e8e7489e7f97db
Rich Cameron <cameron.richard@gmail.com>
2013-05-24 11:46:20 -0400

    Initial Commit

---
+++ RCCollidableViewTestApp/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  RCCollidableViewTestApp
+//
+//  Created by Rich Cameron on 5/24/13.
+//  Copyright (c) 2013 Rich Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
